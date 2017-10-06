feb2a6c84f86ce686ff65a695c195aa03263f7b6
makinoy <makino.y@gmail.com>
2017-10-06 14:57:02 +0900

    Fix sample code

--- tracker-sample/src/main/java/io/karte/android/tracker_sample/MyFirebaseMessagingService.java
+++ tracker-sample/src/main/java/io/karte/android/tracker_sample/MyFirebaseMessagingService.java
@@ -13,10 +13,15 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import io.karte.android.tracker.Tracker;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+  private static final String EXTRA_PUSH_FLAG = "krt_push_notification";
+  private static final String EXTRA_CAMPAIGN_ID = "krt_campaign_id";
+  private static final String EXTRA_SHORTEN_ID = "krt_shorten_id";
 
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -26,14 +31,28 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     if (remoteMessage.getNotification() != null) {
       Log.d(Tracker.LOG_TAG_NAME, "Message Notification Body: " + remoteMessage.getNotification().getBody());
       RemoteMessage.Notification notification = remoteMessage.getNotification();
-      sendNotification(notification.getTitle(), notification.getBody());
+      sendNotification(notification.getTitle(), notification.getBody(), remoteMessage.getData());
+      return;
+    }
+
+    if (remoteMessage.getData() != null) {
+      Map<String, String> data = remoteMessage.getData();
+      String title = data.get("subject");
+      String body = data.get("text");
+      sendNotification(title, body, data);
     }
   }
 
-  protected void sendNotification(String title, String body) {
+  protected void sendNotification(String title, String body, Map<String, String> data) {
 
     Intent intent = new Intent(this, MainActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+    if (data != null) {
+      intent.putExtra(EXTRA_PUSH_FLAG, data.get(EXTRA_PUSH_FLAG));
+      intent.putExtra(EXTRA_CAMPAIGN_ID, data.get(EXTRA_CAMPAIGN_ID));
+      intent.putExtra(EXTRA_SHORTEN_ID, data.get(EXTRA_SHORTEN_ID));
+    }
 
     PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
             PendingIntent.FLAG_ONE_SHOT);
